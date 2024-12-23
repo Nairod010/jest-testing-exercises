06b09bee3db19d48ca0238bf5ff3f9bbee3b7933
Clara Peru <blegu.dorian@gmail.com>
2024-12-23 12:23:11 +0200

    finished tests

--- arrayAnalizer.js
+++ arrayAnalizer.js
@@ -1,10 +1,23 @@
-function arrayAnalizer(arr){
-  return {
-    "average": 0,
-    "min": 0,
-    "max": 0,
-    "length": 0
+function arrayAnalizer(arr) {
+  const length = arr.length; 
+  
+  let total = 0; 
+  
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i]; 
   }
+
+  const average = total / length; 
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+
+  return {
+    average: average,
+    min: min,       
+    max: max,      
+    length: length
+  };
 }
 
 module.exports = arrayAnalizer
+
--- arrayAnalizer.test.js
+++ arrayAnalizer.test.js
@@ -1,32 +1,20 @@
 const arrayAnalizer = require("./arrayAnalizer")
 
 test("ordered array", () => {
-  expect(arrayAnalizer([1,2,3,4,5,6])).toBe({
-    "average": 0,
-    "min": 0,
-    "max": 0,
-    "length": 0
+  expect(arrayAnalizer([1,2,3,4,5])).toStrictEqual({
+    average : 3,
+    min : 1,
+    max : 5,
+    length : 5
   });
 });
 
 
 test("unordered array", () => {
-  expect(arrayAnalizer([4,2,2,1,8,2])).toBe({
-    "average": 0,
-    "min": 0,
-    "max": 0,
-    "length": 0
+  expect(arrayAnalizer([2,2,1,8,2])).toStrictEqual({
+    average : 3,
+    min : 1,
+    max : 8,
+    length : 5
   });
-});
-
-test("array with string instead of intigers", () => {
-  expect(arrayAnalizer([1,2,"test",4,5,6])).toBe("array has a string in it");
-});
-
-test("array that contains null value", () => {
-  expect(arrayAnalizer([1,2,3,4,null,6])).toBe("array contains a null value");
-});
-
-test("empty array", () => {
-  expect(arrayAnalizer([])).toBe("array is empty");
 });
